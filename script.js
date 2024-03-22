c8ee1db6a7e9514cde2e85ae24d3a27ba49c53be
Pramod Kumar Maurya <pramod.maurya12321@gmail.com>
2024-03-23 00:53:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-function convertToCelsius() {
-    var fahrenheitInput = document.getElementById('fahrenheit');
-    var celsiusInput = document.getElementById('celsius');
-    
-    if (fahrenheitInput.value) {
-      var fahrenheit = parseFloat(fahrenheitInput.value);
-      var celsius = (fahrenheit - 32) * 5 / 9;
-      celsiusInput.value = celsius.toFixed(2);
+function convertTemperature() {
+    var inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    var conversionType = document.getElementById("conversionType").value;
+    var outputTemp;
+    switch (conversionType) {
+        case "celsiusToFahrenheit":
+            outputTemp = (inputTemp * 9/5) + 32;
+            break;
+        case "celsiusToKelvin":
+            outputTemp = inputTemp + 273.15;
+            break;
+        case "fahrenheitToCelsius":
+            outputTemp = (inputTemp - 32) * 5/9;
+            break;
+        case "kelvinToCelsius":
+            outputTemp = inputTemp - 273.15;
+            break;
+        default:
+            outputTemp = "Invalid conversion type";
+            break;
     }
-  }
-  
-  function convertToFahrenheit() {
-    var celsiusInput = document.getElementById('celsius');
-    var fahrenheitInput = document.getElementById('fahrenheit');
-    
-    if (celsiusInput.value) {
-      var celsius = parseFloat(celsiusInput.value);
-      var fahrenheit = (celsius * 9 / 5) + 32;
-      fahrenheitInput.value = fahrenheit.toFixed(2);
-    }
-  }
-  
+    document.getElementById("output").innerText = "Converted Value: " + outputTemp;
+}
